af8bfd6f3a7bb47964698207efa3be45f6d2df3a
sebastianVP <alex.valdezp22@gmail.com>
2025-10-20 09:21:39 -0500

    limpieza de datos

--- main_app.py
+++ main_app.py
@@ -8,7 +8,21 @@ def cargar_datos(ruta_archivo):
     print("Datos cargados correctamente.")
     return df
 
+#2. LIMPIEZA DE DATOS
+def clean_datos(df):
+    # Eliminar filas  valores nulos
+    df = df.dropna()
+
+    # Eliminar filas cantidad o precio negativo
+    df = df[(df['cantidad'] > 0) & (df['precio_unitario'] > 0)]
+
+    # Columna 'total'
+    df['total'] = df['cantidad'] * df['precio_unitario']
+
+    print("Datos limpiados correctamente.")
+    return df
 
 if __name__ == "__main__":
     df = cargar_datos("ventas.csv")
+    df = clean_datos(df)
     print(df)
